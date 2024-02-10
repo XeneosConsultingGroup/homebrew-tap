3e107445b078b34ff7f9738b8e1ae33cff133f1a
George Turner <49865096+george-turner@users.noreply.github.com>
2024-02-10 15:18:29 +0000

    update tap

--- Formula/xcg.rb
+++ Formula/xcg.rb
@@ -4,7 +4,7 @@ class Xcg < Formula
   version "0.1.4"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/XeneosConsultingGroup/xcg-cli/releases/download/v0.1.4/xcg_v0.1.4_darwin_arm64.tar.gz"
+    url "https://github.com/XeneosConsultingGroup/xcg-cli/releases/download/v0.1.4/xcg_v0.1.4_darwin_arm64.tar.gz", :using => GitHubReleaseDownloadStrategy
     sha256 "a8e1474fec19023aa083a8eeb926d9ac2d01ae75e0bcded375f47c5faf7df786"
   end
 
