c33b9aad71302f54cbbeeb71cceeb7c13ada718c
xcg-automtation <automtation@xeneos.com>
2024-02-10 19:16:44 +0000

    Bump xcg to v0.1.17

--- Formula/xcg.rb
+++ Formula/xcg.rb
@@ -1,18 +1,13 @@
-product:  xcg
-releaseUrl:  https://github.com/XeneosConsultingGroup/xcg-cli/releases/download/v0.1.16/xcg_v0.1.16_darwin_arm64.tar.gz
-sha:  58ffb9d12a33c4bf1dd1db24b2e151cde2cc02d434316f282fd17ce6f8bf64cb
-version:  v0.1.16
-visibility:  private
 require "formula"
 require_relative "../custom_download_strategy.rb"
 class Xcg < Formula
   desc "xcg"
   homepage "https://software.xeneos.com/internal/cli"
-  version "v0.1.16"
+  version "v0.1.17"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/XeneosConsultingGroup/xcg-cli/releases/download/v0.1.16/xcg_v0.1.16_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "58ffb9d12a33c4bf1dd1db24b2e151cde2cc02d434316f282fd17ce6f8bf64cb"
+    url "https://github.com/XeneosConsultingGroup/xcg-cli/releases/download/v0.1.17/xcg_v0.1.17_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "acf707392157ff38550ea22aaa1ba315a70fb1229d51d119a42634cf98674f74"
   end
 
   conflicts_with "xcg"
