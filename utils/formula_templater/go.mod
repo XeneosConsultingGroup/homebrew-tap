1bfdbc112d78931acc0d22bef87f6e8eb13a5487
George Turner <49865096+george-turner@users.noreply.github.com>
2024-02-10 17:16:14 +0000

    add workflow for updating formulas

---
+++ utils/formula_templater/go.mod
@@ -0,0 +1,3 @@
+module github.com/XeneosConsultingGroup/homebrew-tap/utils/formula_templater
+
+go 1.20
---
+++ utils/formula_templater/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"text/template"
+)
+
+//go:embed templates/private.tmpl
+var privateTemplate string
+
+//go:embed templates/public.tmpl
+var publicTemplate string
+
+type Formula struct {
+	Product    string
+	Name       string
+	ReleaseUrl string
+	SHA256     string
+	Version    string
+	Visibility string
+}
+
+func printFormula(
+	product string,
+	releaseUrl string,
+	sha string,
+	version string,
+	visibility string,
+	out io.Writer,
+) error {
+	formulaName := strings.ToUpper(
+		strings.Split(product, "")[0],
+	) + strings.Join(
+		strings.Split(product, "")[1:],
+		"",
+	)
+	formulaConfig := Formula{
+		Product:    product,
+		Name:       formulaName,
+		ReleaseUrl: releaseUrl,
+		SHA256:     sha,
+		Version:    version,
+		Visibility: visibility,
+	}
+
+	t := template.Must(template.New("Formula").Parse(privateTemplate))
+	return t.Execute(out, formulaConfig)
+}
+
+func main() {
+	i := 0
+	product := os.Args[i+1]
+	releaseUrl := os.Args[i+2]
+	sha := os.Args[i+3]
+	version := os.Args[i+4]
+	visibility := os.Args[i+5]
+
+	fmt.Println("product: ", product)
+	fmt.Println("releaseUrl: ", releaseUrl)
+	fmt.Println("sha: ", sha)
+	fmt.Println("version: ", version)
+	fmt.Println("visibility: ", visibility)
+
+	err := printFormula(product, releaseUrl, sha, version, visibility, os.Stdout)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
