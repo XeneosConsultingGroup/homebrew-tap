cce0c25eaa12f3a6506a10472bba2068a6fd2f90
George Turner <49865096+george-turner@users.noreply.github.com>
2024-02-10 14:55:40 +0000

    initial formula

---
+++ Formula/xcg.rb
@@ -0,0 +1,20 @@
+class XCG < Formula
+  desc "xcg"
+  homepage "https://software.xeneos.com/internal/cli"
+  version "0.1.4"
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/XeneosConsultingGroup/xcg-cli/releases/download/v0.1.4/xcg_v0.1.4_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "a8e1474fec19023aa083a8eeb926d9ac2d01ae75e0bcded375f47c5faf7df786"
+  end
+
+  conflicts_with "xcg"
+
+  def install
+    bin.install "xcg"
+  end
+
+  test do
+    system "#{bin}/xcg --version"
+  end
+end
