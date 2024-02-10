137302ad14b132c845b3e30043369121a5424e01
George Turner <49865096+george-turner@users.noreply.github.com>
2024-02-10 15:51:12 +0000

    update xcg-cli version to 0.1.6

--- Formula/xcg.rb
+++ Formula/xcg.rb
@@ -3,11 +3,11 @@ require_relative "../custom_download_strategy.rb"
 class Xcg < Formula
   desc "xcg"
   homepage "https://software.xeneos.com/internal/cli"
-  version "0.1.4"
+  version "0.1.6"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/XeneosConsultingGroup/xcg-cli/releases/download/v0.1.4/xcg_v0.1.4_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "a8e1474fec19023aa083a8eeb926d9ac2d01ae75e0bcded375f47c5faf7df786"
+    url "https://github.com/XeneosConsultingGroup/xcg-cli/releases/download/v0.1.6/xcg_v0.1.6_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "112c471429f7263b30dd7548f5a877eb8c5fdbfd3074336d86657a84e03587eb"
   end
 
   conflicts_with "xcg"
